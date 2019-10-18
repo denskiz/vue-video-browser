b81402b3df45fe7795021ec28a3720ad449571e0
Dennis <dennisent@protonmail.com>
2019-10-19 07:44:21 +1100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Video Search</h1>
+    <h5>Search for Videos - Built with Vue.js & the YouTube API</h5>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <div class="row">
+      <VideoDetail :video="selectedVideo" />
+      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+const API_KEY = "AIzaSyAeIB5Shr8P14DsDhSfiuNllzeDBEFc6Fk";
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data() {
+    return { videos: [], selectedVideo: null };
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items;
+        });
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 17px;
+  font-family: Ubuntu, Cantarell, Helvetica Neue, sans-serif;
+}
+h5 {
+  text-align: center;
+  font-size: 17px;
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input @input="onInput" class="form-control" placeholder="Search for..." />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      this.$emit("termChange", event.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  width: 60%;
+  margin: auto;
+}
+div {
+  text-align: center;
+  margin: 20px;
+}
+</style>
